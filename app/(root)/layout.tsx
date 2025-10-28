95a1739233ac8848162b855e9692001081031ee7
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-10-28 20:13:27 +0300

    layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import '../globals.css'
+import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -13,12 +14,15 @@ export const metadata: Metadata = {
     "A social media app , to discover what is happing now in the world",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
+
+  const user = await currentUser()
+  if(!user){
+     return (
     <ClerkProvider>
       <html lang="en">
         <body>
@@ -31,4 +35,24 @@ export default function RootLayout({
       </html>
     </ClerkProvider>
   );
+  }
+ 
+   return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <main className={`${inter.className} `}>
+            <TopBar />
+            <section className="main-container">
+              <div className="flex items-center justify-center w-full min-h-screen">
+                {children}
+              </div>
+            </section>
+            <BottomBar/>
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import LandingPage from "@/components/shared/LandingPage";
 import { currentUser } from "@clerk/nextjs/server";
+import "../globals.css"
 
 export default async function Home() {
 
