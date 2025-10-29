482f82be17d89a3bfaa22c80f47ee115d0cae7dc
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-10-29 11:07:26 +0300

    add webhooks for create new user

---
+++ app/api/webhooks/clerk/route.ts
@@ -0,0 +1,33 @@
+import { createUser } from '@/lib/actions/user.action'
+import { verifyWebhook } from '@clerk/nextjs/webhooks'
+import { NextRequest } from 'next/server'
+
+export async function POST(req: NextRequest) {
+  try {
+    const evt = await verifyWebhook(req)
+
+    // Do something with payload
+    // For this guide, log payload to console
+    // const { id } = evt.data
+    // const eventType = evt.type
+    // console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
+    // console.log('Webhook payload:', evt.data)
+
+    if(evt.type === 'user.created'){
+        const user = evt.data
+        await createUser({
+            id: user.id,
+            email:user.email_addresses[0].email_address,
+            name:`${user.first_name || ""} ${user.last_name || ""}`,
+            username: user.username || "",
+            image: user.image_url || "",
+            
+        })
+    }
+
+    return new Response('Webhook received', { status: 200 })
+  } catch (err) {
+    console.error('Error verifying webhook:', err)
+    return new Response('Error verifying webhook', { status: 400 })
+  }
+}
